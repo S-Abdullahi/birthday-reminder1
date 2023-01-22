dd8b86b1853312fb8d723be22f039e56b1589458
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-01-23 00:22:37 +0100

    final commit

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+import Data from '../data'
+
+
 
 export default function Main() {
+
+  const [data, setData]  = useState(Data)
+
+  const birthday = data.map((d)=>{
+    return (
+    <div className="card-item" key={d.id}>
+    <img src={d.image} alt="pic" className="card-image"/>
+    <div className="card-detail">
+      <span className="card-name">{d.name}</span>
+      <span className="card-year">{d.age}</span>
+    </div>
+  </div>)
+  })
+
+  function clearData(){
+    setData([])
+  }
+
   return (
     <div className="card">
-      <h2 className="card-count"> 5 birthday today</h2>
-      <div className="card-item">
-        <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="pic" className="card-image"/>
-        <div className="card-detail">
-          <span card-name>Salawu Abdullahi</span>
-          <span className="card-year">26 years</span>
-        </div>
-      </div>
-      <button>Clear all</button>
+      <h2 className="card-count"> {data.length} birthday today</h2>
+      {birthday}
+      <button onClick={clearData}>Clear all</button>
     </div>
   );
 }
